acb9941af26e3224db57f90523c3cf91c476fb48
Frank A. <acki77.github@digital-filestore.de>
2026-02-23 14:39:40 +0100

    Update pages/api/flashcards/index.js
    insert errorhandling
    
    Co-authored-by: Stille Klaus <mail@klausstille.com>

--- pages/api/flashcards/index.js
+++ pages/api/flashcards/index.js
@@ -4,8 +4,14 @@ import Flashcards from "@/db/models/Flashcards";
 export default async function handler(request, response) {
   await dbConnect();
 
-  if (request.method === "GET") {
-    const flashcards = await Flashcards.find();
-    return response.status(200).json(flashcards);
-  }
+    if (request.method === "GET") {
+        try {
+            const flashcards = await Flashcards.find();
+            return response.status(200).json(flashcards);
+        } catch(error){
+            return response.status(500).json({ error: error.message });
+        }
+    } else {
+        return response.status(405).json({ message: "Method not allowed" });
+    }
 }
