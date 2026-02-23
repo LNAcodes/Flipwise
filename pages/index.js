1b8a39d63c80cdde149fadba56348dce59cc2666
Frank A. <acki77.github@digital-filestore.de>
2026-02-23 14:38:36 +0100

    Update pages/index.js
    change container div to p
    
    Co-authored-by: Stille Klaus <mail@klausstille.com>

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ import useSWR from "swr";
 const fetcher = (url) => fetch(url).then((res) => res.json());
 export default function HomePage() {
   const { data, error, isLoading } = useSWR("/api/flashcards", fetcher);
-  if (error) return <div>Fehler beim Laden</div>;
-  if (isLoading) return <div>Lade Daten...Bitte warten...</div>;
+  if (error) return <p>Fehler beim Laden</p>;
+  if (isLoading) return <p>Lade Daten...Bitte warten...</p>;
 
   return (
     <>
