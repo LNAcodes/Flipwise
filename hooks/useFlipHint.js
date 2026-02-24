a4a863d746e4a40ba41277c332f8ba5ccfd052a6
Karsten Weng <karsten@weng.eu>
2026-02-24 14:42:09 +0100

    implement localstorage to hook

--- hooks/useFlipHint.js
+++ hooks/useFlipHint.js
@@ -1,11 +1,23 @@
 // hooks\useFlipHint.js
 
-import { useState } from "react";
+// speicher state im localstorage, so das nach reload noch vorhanden
+import useLocalStorageState from "use-local-storage-state";
+
+// name des states im localStorage
+const STORAGE_KEY = "flashcards-ui";
 
 export function useFlipHint() {
-  const [ui, setUi] = useState({ hasFlipped: false });
+  // ui = object , das den aktueller Zustand speichert
+  // setUi = funktion, die den state ändert
+  // defaultValue = wenn noch nicht localstorage gespeichert nimm 'false'
+  const [ui, setUi] = useLocalStorageState(STORAGE_KEY, {
+    defaultValue: { hasFlipped: false },
+  });
+
+  const showHint = !ui.hasFlipped; // kann true oder false sein. bei true zeige hint bei false nicht
 
   function markFirstFlip() {
+    // prev =  bisherige zustand aus localstorage/state
     setUi((prev) => {
       // wenn schon mal geflipt wurde, mache nichts
       if (prev.hasFlipped === true) {
@@ -20,9 +32,5 @@ export function useFlipHint() {
     });
   }
 
-  return {
-    ui,
-    showHint: !ui.hasFlipped,
-    markFirstFlip,
-  };
+  return { ui, showHint, markFirstFlip };
 }
