b727a7a16581b01cc554b84b13cc876de58657eb
Karsten Weng <karsten@weng.eu>
2026-02-25 15:11:18 +0100

    update flascards api route

--- pages/api/flashcards/index.js
+++ pages/api/flashcards/index.js
@@ -1,5 +1,7 @@
 // pages\api\flashcards\index.js
 
+// pages/api/flashcards/index.js
+
 import dbConnect from "@/db/connect";
 import Flashcard from "@/db/models/Flashcard";
 
@@ -13,16 +15,18 @@ export default async function handler(request, response) {
     } catch (error) {
       return response.status(500).json({ error: error.message });
     }
-  } else if (request.method === "POST") {
+  }
+
+  if (request.method === "POST") {
     const flashcardData = request.body;
+
     try {
       const flashcard = await Flashcard.create(flashcardData);
       return response.status(201).json(flashcard);
     } catch (error) {
-      console.error(error);
-      return response.status(500).json({ status: error.message });
+      return response.status(500).json({ error: error.message });
     }
-  } else {
-    return response.status(405).json({ message: "Method not allowed" });
   }
+
+  return response.status(405).json({ message: "Method not allowed" });
 }
