350ba6238572b65ad6da0de360ef045fe0085278
Karsten Weng <karsten@weng.eu>
2026-02-26 19:25:08 +0100

    make footer sticky

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,10 +1,21 @@
 // components/Footer/Footer.js
+import styled from "styled-components";
+
+const StyledFooter = styled.footer`
+  position: sticky;
+  bottom: 0;
+  z-index: 10;
+  display: flex;
+  justify-content: center;
+  background-color: var(--bg-color-light);
+  padding: 16px 24px;
+`;
 
 export default function Footer() {
   return (
-    <footer>
+    <StyledFooter>
       <p>Footer</p>
       {/* <Navbar /> */}
-    </footer>
+    </StyledFooter>
   );
 }
