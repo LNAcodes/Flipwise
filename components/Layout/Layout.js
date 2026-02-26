16281f6f2cbe851a53dce391c3ed51763e25d501
Karsten Weng <karsten@weng.eu>
2026-02-26 16:23:28 +0100

    add 'Layout' component

---
+++ components/Layout/Layout.js
@@ -0,0 +1,22 @@
+// components/Layout/Layout.js
+import styled from "styled-components";
+import Header from "@/components/Header/Header";
+
+const Wrapper = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Main = styled.main`
+  flex: 1;
+`;
+
+export default function Layout({ children }) {
+  return (
+    <Wrapper>
+      <Header />
+      <Main>{children}</Main>
+    </Wrapper>
+  );
+}
