7ee243369401ab1288efa0871d0de9f5a1ff0026
Karsten Weng <karsten@weng.eu>
2026-02-26 21:02:29 +0100

    add navi icons and make some styling

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,5 +1,12 @@
 import Link from "next/link";
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faListUl,
+  faPlusCircle,
+  faPlay,
+} from "@fortawesome/free-solid-svg-icons";
 
 const NavBar = styled.nav`
   display: flex;
@@ -10,28 +17,68 @@ const List = styled.ul`
   margin: 0;
   padding: 0;
   list-style: none;
-
   display: flex;
-  gap: 16px;
+  gap: 30px;
 `;
 
-const Item = styled.li``;
+const Item = styled.li`
+  padding: 0;
+`;
+
+const StyledLink = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 6px;
+  width: 100%;
+  padding: 10px;
+  text-align: center;
+  color: var(--color-primary);
+  text-decoration: none;
+  min-height: 44px;
+
+  &:hover {
+    color: #313131;
+  }
+`;
+const Icon = styled(FontAwesomeIcon)`
+  width: 30px;
+  height: 30px;
+  max-width: none;
+  flex: 0 0 auto;
+`;
+
+const LinkText = styled.span`
+  font-size: 12px;
+`;
 
 export default function Navbar() {
   return (
     <NavBar aria-label="Primary">
       <List>
         <Item>
-          <Link href="/">Home</Link>
+          <StyledLink href="/" aria-label="Go to starpage">
+            <Icon icon={faHome} aria-hidden="true" />
+            <LinkText>Home</LinkText>
+          </StyledLink>
         </Item>
         <Item>
-          <Link href="/flashcards">Card List</Link>
+          <StyledLink href="/flashcards/" aria-label="Go to card list page">
+            <Icon icon={faListUl} aria-hidden="true" />
+            <LinkText>Card List</LinkText>
+          </StyledLink>
         </Item>
         <Item>
-          <Link href="/add-card/">Add Card</Link>
+          <StyledLink href="/add-card/" aria-label="Go to add card page">
+            <Icon icon={faPlusCircle} aria-hidden="true" />
+            <LinkText>Add Card</LinkText>
+          </StyledLink>
         </Item>
         <Item>
-          <Link href="/quiz/">Quiz</Link>
+          <StyledLink href="/quiz/" aria-label="Go to add card page">
+            <Icon icon={faPlay} aria-hidden="true" />
+            <LinkText>Quiz</LinkText>
+          </StyledLink>
         </Item>
       </List>
     </NavBar>
