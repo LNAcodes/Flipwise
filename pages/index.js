ed8ecfdf2f469897f1c27fb62aab284af45ef186
Frank Ackermann <acki77.github@digital-filestore.de>
2026-02-26 02:09:19 +0100

    add new useSWR for collection to read color from db

--- pages/index.js
+++ pages/index.js
@@ -13,15 +13,35 @@ const Title = styled.h1`
 `;
 
 export default function HomePage() {
-  const { data, error, isLoading } = useSWR("/api/flashcards");
-  if (error) return <p>Error loading</p>;
-  if (isLoading) return <p>Loading data... Please wait...</p>;
+  const {
+    data: flashcards,
+    error: errorCards,
+    isLoading: loadingCards,
+  } = useSWR("/api/flashcards");
+  const {
+    data: collections,
+    error: erroColls,
+    isLoading: loadingColls,
+  } = useSWR("/api/collections");
+
+  if (errorCards || erroColls) return <p>Error loading</p>;
+  if (loadingCards || loadingColls)
+    return <p>Loading data... Please wait...</p>;
+  const enrichedFlashcards = flashcards.map((card) => {
+    const matchingCollection = collections.find(
+      (col) => col.name === card.collection
+    );
+    return {
+      ...card,
+      color: matchingCollection ? matchingCollection.color : "#defaultColor#",
+    };
+  });
 
   return (
     <main>
       <Title>Homepage</Title>
       <FlashCardForm />
-      <FlashCardList flashcards={data} />
+      <FlashCardList flashcards={enrichedFlashcards} />
     </main>
   );
 }
