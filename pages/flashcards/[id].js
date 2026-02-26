dec8ca999bffea83374fd7b1cd7991f7730f9da6
Karsten Weng <karsten@weng.eu>
2026-02-26 12:41:53 +0100

    fetch detail card only when id exists

--- pages/flashcards/[id].js
+++ pages/flashcards/[id].js
@@ -3,33 +3,30 @@
 import useSWR from "swr";
 import { useRouter } from "next/router";
 import FlashCardForm from "@/components/FlashCardForm/FlashCardForm";
-import { useSWRConfig } from "swr";
 
 export default function FlashCardDetailPage() {
   const router = useRouter();
   const { id } = router.query;
-  const { mutate } = useSWRConfig();
 
-  const { data, error, isLoading } = useSWR(
-    id ? `/api/flashcards/${id}` : null
-  );
+  // nur fetchen wenn route 'id' existiet, sonst gib 'null' weiter
+  const flashcardUrl = id ? `/api/flashcards/${id}` : null;
+  const { data, error, isLoading, mutate } = useSWR(flashcardUrl);
 
   if (error) return <p>Error loading</p>;
   if (isLoading || !id) return <p>Loading data... Please wait...</p>;
   if (!data) return <h1>FlashCard not found</h1>;
 
   async function handleUpdate(updatedData) {
-    const res = await fetch(`/api/flashcards/${id}`, {
+    const result = await fetch(`/api/flashcards/${id}`, {
       method: "PATCH",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(updatedData),
     });
 
-    if (!res.ok) return;
+    if (!result.ok) return;
 
-    const updated = await res.json();
-    mutate(`/api/flashcards/${id}`, updated, false);
-    mutate("/api/flashcards");
+    const updated = await result.json();
+    mutate(updated, false);
 
     router.push("/flashcards");
   }
