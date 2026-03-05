b49072445f00d900b251ac074cedc9251ddaf5c0
Frank Ackermann <acki77.github@digital-filestore.de>
2026-03-05 12:38:40 +0100

    test quiz Selection logic

--- components/QuizSetup/index.js
+++ components/QuizSetup/index.js
@@ -1,13 +1,22 @@
 //components/QuizSetup/index.js
-import { getRandomQuizCards } from "@/utils/quizHelpers";
+import {
+  getRandomQuizCards,
+  getCollectionQuizCards,
+} from "@/utils/quizHelpers";
 
-const Message = styled.h2`
+/* const Message = styled.h2`
   color: #f10707;
   font-size: 20px;
-`;
+`; */
+console.log("Dateitest: QUizSetup wurde geladen!");
 
 export default function QuizSetup({ allCards }) {
-  const quizCards = getRandomQuizCards(allCards);
+  console.log("Alle Karten:", allCards);
+  console.log("Zufall 10:", getRandomQuizCards(allCards));
+  console.log("Collection 'React':", getCollectionQuizCards(allCards, "React"));
+
+  return <div>Setup View (Coming Soon)</div>;
+  /* const quizCards = getRandomQuizCards(allCards);
 
   return (
     <>
@@ -20,5 +29,5 @@ export default function QuizSetup({ allCards }) {
         <button onClick={handleStart}>Start 10-Card-Quiz</button>
       )}
     </>
-  );
+  ); */
 }
--- pages/quiz/index.js
+++ pages/quiz/index.js
@@ -1,20 +1,44 @@
 // pages/quiz/index.js
-
+import QuizSetup from "@/components/QuizSetup";
 import styled from "styled-components";
-
+import useSWR from "swr";
 const PageTitle = styled.h1`
   padding: 0;
 `;
 
 export default function QuizPage() {
+  const {
+    data: flashcards,
+    error: cardsError,
+    isLoading: cardsLoading,
+  } = useSWR("/api/flashcards");
+  const {
+    data: collections,
+    error: collectionsError,
+    isLoading: colectionsLoading,
+  } = useSWR("/api/collections");
+
+  if (cardsError || collectionsError) return <p>Error loading</p>;
+  if (cardsLoading || colectionsLoading)
+    return <p>Loading data... Please wait...</p>;
+  const enrichedFlashcards = flashcards.map((card) => {
+    const matchingCollection = collections.find(
+      (col) => col.name === card.collection
+    );
+    return {
+      ...card,
+      color: matchingCollection ? matchingCollection.color : "#defaultColor#",
+    };
+  });
   return (
     <>
-      <PageTitle>Quiz</PageTitle>
+      <PageTitle>Card List</PageTitle>
+      <QuizSetup allCards={enrichedFlashcards} collection={collections} />
     </>
   );
 }
 
 QuizPage.meta = {
-  title: "Quiz",
-  description: "Start quiz and learn cards.",
+  title: "Card List",
+  description: "A list of all your cards.",
 };
